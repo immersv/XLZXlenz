1426ea216db47be1fd10de890c139a5578203303
immersvin <immersv@gmail.com>
2020-06-22 16:11:44 +0530

    Vuforia intialized

---
+++ Assets/XIDScan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XIDScan : MonoBehaviour
+{
+    public int Xid;
+
+    void XidFound()
+    {
+
+    }
+    void XidNotFound()
+    {
+
+    }
+}
